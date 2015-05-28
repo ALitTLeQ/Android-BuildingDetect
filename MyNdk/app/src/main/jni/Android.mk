b1290c3e97d5a8349ae1f211579afe1a9089b040
ALitTLeQ <ee830804ee@gmail.com>
2015-05-28 16:23:46 +0800

    Revert "add nonfree module"
    This reverts commit db91ee709ebddbca0b106ce4aea460e30cbd732a.

--- MyNdk/app/src/main/jni/Android.mk
+++ MyNdk/app/src/main/jni/Android.mk
@@ -9,12 +9,8 @@ OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 include C:/OpenCV-2.4.10-android-sdk/sdk/native/jni/OpenCV.mk
 
-LOCAL_SRC_FILES := :=	nonfree_init.cpp			\
-                        sift.cpp					\
-                        surf.cpp                    \
-                        main.cpp
-LOCAL_LDLIBS += -llog -ldl
+LOCAL_SRC_FILES := main.cpp
+LOCAL_LDLIBS += -llog
 LOCAL_MODULE := MyLib
-LOCAL_CFLAGS := -Werror -O3 -ffast-math
 
 include $(BUILD_SHARED_LIBRARY)
